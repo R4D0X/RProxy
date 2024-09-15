79e21279731dc30c11f3489b71a1e65f3ac1ed45
R4D0X <emretilbe90@gmail.com>
2024-09-15 18:47:05 +0300

    Uploading files.

---
+++ installer.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import platform
+from colorama import init, Fore, Style
+init(autoreset=True)
+
+def install_dependencies():
+    try:
+        print(f"{Fore.LIGHTCYAN_EX}------------------------------------------------")
+        print(f"{Fore.CYAN}[{Fore.YELLOW}-{Fore.CYAN}]{Fore.WHITE} Gerekli paketler yükleniyor...")
+        print(f"{Fore.LIGHTCYAN_EX}------------------------------------------------")
+        subprocess.run([get_pip_command(), 'install', '--upgrade', 
+                        'requests', 'colorama', 'proxy_checker', 'tqdm', 
+                        'tabulate', '--break-system-packages'], check=True)
+        print(f"{Fore.LIGHTCYAN_EX}------------------------------------------------")
+        print(f"{Fore.CYAN}[{Fore.GREEN}+{Fore.CYAN}]{Style.RESET_ALL} Paketler başarıyla yüklendi.")
+        print(f"{Fore.LIGHTCYAN_EX}------------------------------------------------")
+    except subprocess.CalledProcessError as e:
+        print(f"{Fore.LIGHTCYAN_EX}------------------------------------------------")
+        print(f"{Fore.CYAN}[{Fore.RED}x{Fore.CYAN}]{Fore.WHITE} Paketler yüklenirken hata oluştu: {e}")
+        print(f"{Fore.LIGHTCYAN_EX}------------------------------------------------")
+
+def get_pip_command():
+    if platform.system().lower() == 'windows':
+        return 'pip'
+    else:
+        return 'pip3'
+
+def get_python_command():
+    if platform.system().lower() == 'windows':
+        return 'python'
+    else:
+        return 'python3'
+
+def clear_screen():
+    os.system('cls' if platform.system() == "Windows" else 'clear')
+
+def main():
+    init()
+    clear_screen()
+    init(autoreset=True)
+    print(f"{Fore.LIGHTYELLOW_EX}{Style.BRIGHT} RProxy Installer")
+
+    if os.name != 'nt' and hasattr(os, 'geteuid') and os.geteuid() == 0:
+        print(f"{Fore.CYAN}[{Fore.RED}x{Fore.CYAN}]{Style.RESET_ALL} HATA, PROGRAMI YÖNETİCİ OLARAK ÇALIŞTIRMALISINIZ.")
+        return
+
+    install_dependencies()
+
+if __name__ == "__main__":
+    main()
---
+++ rproxy.py
@@ -0,0 +1,276 @@
+import os
+from colorama import Fore, init, Style
+import requests
+import re
+import platform
+import time
+import threading
+from proxy_checker import ProxyChecker
+from sys import stdout
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+lock = threading.Lock()
+defs,sett = 1 - 1, 1 + 3
+class UI:
+    @staticmethod
+    def menu():
+        menu = f'''
+                            █▀▀█ █▀▀█ █▀▀▄ █▀▀█ █░█
+                            █▄▄▀ █▄▄█ █░░█ █░░█ ▄▀▄
+                            ▀░▀▀ ▀░░▀ ▀▀▀░ ▀▀▀▀ ▀░▀     {Fore.CYAN}https://t.me/radoxin
+           
+           {Fore.RED}[{Fore.YELLOW}1{Fore.RED}]{Style.RESET_ALL} HTTP Proxy Topla    {Fore.RED}[{Fore.YELLOW}2{Fore.RED}]{Style.RESET_ALL} SOCKS4-SOCKS5 Proxy Topla  
+            
+                            {Fore.RED}[{Fore.YELLOW}3{Fore.RED}]{Style.RESET_ALL} Proxy Listesi Kontrol Et 
+        '''
+        return menu
+mdb = 'D'
+def write(arg):
+    with lock:
+        stdout.flush()
+        print(arg)
+
+class ProxyChecker:
+    def check_proxy(self, proxy):
+        proxies = {
+            'http': f'http://{proxy}',
+            'https': f'http://{proxy}',  
+        }
+        try:
+            response = requests.get('http://httpbin.org/ip', proxies=proxies, timeout=1)  
+            return response.status_code == 200
+        except requests.RequestException as e:
+            write(f'{Fore.RED}[-] {Style.RESET_ALL}Proxy kontrol hatası: {e}')
+            return False
+xer = f'{Fore.RED}Made By'
+x = f'{Fore.MAGENTA}'
+class XProxy:
+    proxy_w_regex = [
+        ["http://spys.me/proxy.txt", "%ip%:%port% "],
+        ["http://www.httptunnel.ge/ProxyListForFree.aspx", ' target="_new">%ip%:%port%</a>'],
+        ["https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=6000&country=all&ssl=yes&anonymity=all", "%ip%:%port%"],
+        ['http://free-fresh-proxy-daily.blogspot.com/feeds/posts/default', "%ip%:%port%"],
+        ['http://www.proxyserverlist24.top/feeds/posts/default', "%ip%:%port%"],
+        ['http://proxysearcher.sourceforge.net/Proxy%20List.php?type=http',"%ip%:%port%"],
+        ['http://proxysearcher.sourceforge.net/Proxy%20List.php?type=socks', "%ip%:%port%"], 
+        ['https://www.my-proxy.com/free-anonymous-proxy.html', '%ip%:%port%'],
+        ['https://www.my-proxy.com/free-transparent-.html', '%ip%:%port%'],
+        ['https://www.my-proxy.com/free-proxy-list.html','%ip%:%port%'],
+        ['https://www.my-proxy.com/free-proxy-list-2.html','%ip%:%port%'],
+        ['https://free-proxy-list.net/', '%ip%:%port%'],
+        ['https://www.us-proxy.org/', '%ip%:%port%'],
+        ['https://www.proxy-list.download/HTTP', '%ip%:%port%'],
+        ['https://www.proxy-list.download/HTTPS', '%ip%:%port%'],
+        ['https://hidemy.name/en/proxy-list/', '%ip%:%port%'],
+        ['https://www.proxy-list.org/en/', '%ip%:%port%'],
+        ['https://www.proxynova.com/proxy-server-list/', '%ip%:%port%'],
+        ['https://www.proxy-list.biz/', '%ip%:%port%'],
+        ['https://www.webshare.io/', '%ip%:%port%'],
+        ['https://free-proxy-list.net/uk-proxy.html', '%ip%:%port%'],
+        ['https://www.proxynova.com/proxy-server-list/country-us/', '%ip%:%port%'],
+        ['https://free-proxy-list.net/anonymous-proxy.html', '%ip%:%port%'],
+        ['https://www.proxynova.com/proxy-server-list/country-br/', '%ip%:%port%'],
+        ['https://www.proxynova.com/proxy-server-list/country-cn/', '%ip%:%port%'],
+        ['https://www.proxynova.com/proxy-server-list/country-ru/', '%ip%:%port%'],
+        ['https://www.proxygather.com/', '%ip%:%port%'],
+        ['https://www.proxy-listen.de/azenv.php', '%ip%:%port%'],
+        ['https://www.freeproxylists.net/', '%ip%:%port%'],
+        ['https://proxy-list.org/english/index.php', '%ip%:%port%'],
+        ['https://www.proxy-list.org/', '%ip%:%port%'],
+        ['https://www.proxy4free.com/', '%ip%:%port%'],
+        ['https://www.proxybazaar.com/', '%ip%:%port%'],
+        ['https://www.proxyrack.com/', '%ip%:%port%'],
+        ['https://www.proxy-list.download/', '%ip%:%port%'],
+        ['https://proxylist.me/', '%ip%:%port%'],
+        ['https://www.us-proxy.org/', '%ip%:%port%'],
+        ['https://free-proxy-list.net/', '%ip%:%port%'],
+        ['https://www.proxynova.com/proxy-server-list/country-fr/', '%ip%:%port%'],
+        ['https://www.proxynova.com/proxy-server-list/country-de/', '%ip%:%port%'],
+        ['https://www.my-proxy.com/free-proxy-list-3.html','%ip%:%port%'],
+        ['https://www.my-proxy.com/free-proxy-list-4.html', '%ip%:%port%'],
+        ['https://www.my-proxy.com/free-proxy-list-5.html','%ip%:%port%'],
+        ['https://www.my-proxy.com/free-proxy-list-6.html','%ip%:%port%'],
+        ['https://www.my-proxy.com/free-proxy-list-7.html','%ip%:%port%'],
+        ['https://www.my-proxy.com/free-proxy-list-8.html','%ip%:%port%'],
+        ['https://www.my-proxy.com/free-proxy-list-9.html','%ip%:%port%'],
+        ['https://www.my-proxy.com/free-proxy-list-10.html','%ip%:%port%'],
+        ['https://spys.one/en/free-proxy-list/', '%ip%:%port%'],
+        ['https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=1000&country=all&ssl=all','%ip%:%port%']
+        
+    ]
+
+    socks_proxy_list = [
+        ['https://www.socks-proxy.net/', "%ip%:%port%"],
+        ['http://www.socks24.org/feeds/posts/default', "%ip%:%port%"],
+        ['https://www.freeproxychecker.com/result/socks4_proxies.txt', "%ip%:%port%"],
+        ['https://www.my-proxy.com/free-socks-4-proxy.html', '%ip%:%port%'],
+        ['https://www.my-proxy.com/free-socks-5-proxy.html','%ip%:%port%'],
+        ['https://proxyscrape.com/free-proxy-list', '%ip%:%port%'],
+        ['https://www.sslproxies.org/socks-proxy-list/', '%ip%:%port%'],
+        ['https://spys.one/en/socks-proxy-list/', '%ip%:%port%'],
+        ['https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=1000&country=all&ssl=all','%ip%:%port%']
+    ]
+    proxy_direct = [
+        'https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=5000&country=all&ssl=all&anonymity=all',
+        'https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=5000&country=all&ssl=all&anonymity=all',         
+    ]
+
+    def __init__(self):
+        self.scrape_counter = 0
+        self.checked_counter = 0
+
+    def proxy_scrape(self):
+        start = time.time()
+        proxylist = []
+        for url, regex in XProxy.proxy_w_regex:
+            try:
+                write(f'{Fore.RED}[*] {Style.RESET_ALL}Proxies alınıyor: {Fore.YELLOW}{url}{Style.RESET_ALL}')
+                html = requests.get(url).text
+                iplist = re.findall(r'' + regex.replace('%ip%', '([0-9]{1,3}(?:\.[0-9]{1,3}){3})').replace('%port%', '([0-9]{2,5})') + '', str(html))
+                for (ip, port) in iplist:
+                    proxy = f'{ip}:{port}'
+                    if proxy not in proxylist:
+                        write(f'{Fore.RED}[+] {Style.RESET_ALL}Bulunan proxy: {Fore.GREEN}{proxy}{Style.RESET_ALL}')
+                        proxylist.append(proxy)
+                        self.scrape_counter += 1
+            except Exception as e:
+                write(f'{Fore.RED}[-] {Style.RESET_ALL}Hata: {e}')
+
+        with open("proxies.txt", "a") as file:
+            for proxy in proxylist:
+                file.write(f"{proxy}\n")
+
+        elapsed = time.time() - start
+        set_title(f'Proxy - geçen süre: {elapsed:.2f} saniye | toplanan: {self.scrape_counter} proxy')
+        write(f'{Fore.RED}[*] {Style.RESET_ALL}Proxies proxies.txt dosyasına kaydedildi.')
+    
+    def socks_proxy_scrape(self):
+        start = time.time()
+        proxylist = []
+        for url, regex in XProxy.socks_proxy_list:
+            try:
+                write(f'{Fore.RED}[*] {Style.RESET_ALL}Proxies alınıyor: {Fore.YELLOW}{url}{Style.RESET_ALL}')
+                html = requests.get(url).text
+                iplist = re.findall(r'' + regex.replace('%ip%', '([0-9]{1,3}(?:\.[0-9]{1,3}){3})').replace('%port%', '([0-9]{2,5})') + '', str(html))
+                for (ip, port) in iplist:
+                    proxy = f'{ip}:{port}'
+                    if proxy not in proxylist:
+                        write(f'{Fore.RED}[+] {Style.RESET_ALL}Bulunan proxy: {Fore.GREEN}{proxy}{Style.RESET_ALL}')
+                        proxylist.append(proxy)
+                        self.scrape_counter += 1
+            except Exception as e:
+                write(f'{Fore.RED}[-] {Style.RESET_ALL}Hata: {e}')
+
+        for url in XProxy.proxy_direct:
+            try:
+                write(f'{Fore.RED}[*] {Style.RESET_ALL}Proxies alınıyor: {Fore.YELLOW}{url}{Style.RESET_ALL}')
+                proxies = requests.get(url).text.split()
+                for proxy in proxies:
+                    if proxy not in proxylist:
+                        write(f'{Fore.RED}[+] {Style.RESET_ALL}Bulunan proxy: {Fore.GREEN}{proxy}{Style.RESET_ALL}')
+                        proxylist.append(proxy)
+                        self.scrape_counter += 1
+            except Exception as e:
+                write(f'{Fore.RED}[-] {Style.RESET_ALL}Hata: {e}')
+
+        with open("socks_proxies.txt", "a") as file:
+            for proxy in proxylist:
+                file.write(f"{proxy}\n")
+
+        elapsed = time.time() - start
+        set_title(f'Proxy - geçen süre: {elapsed:.2f} saniye | toplanan: {self.scrape_counter} proxy')
+        write(f'{Fore.RED}[*] {Style.RESET_ALL}Proxies socks_proxies.txt dosyasına kaydedildi.')
+    def proxy_check(self):
+        checker = ProxyChecker()
+        proxylist = []
+        retries = 2
+        while True:
+            path = input(f'{Fore.YELLOW}[+] {Style.RESET_ALL}Proxy liste yolu girin: ').strip('"')
+            if os.path.isfile(path):
+                break
+            else:
+                print(f"{Fore.MAGENTA}[-] {Fore.RED}Dosya yanlış, lütfen geçerli bir dosya yolu girin.{Style.RESET_ALL}")
+
+        with open(path, 'r') as file:
+            proxylist = file.readlines()
+
+        start = time.time()
+        valid_proxies = []
+
+        def check_proxy(proxy):
+            attempt = 0
+            while attempt < retries:
+                try:
+                    if checker.check_proxy(proxy):
+                        write(f'{Fore.RED}[+] {Style.RESET_ALL}Geçerli proxy: {Fore.GREEN}{proxy}{Style.RESET_ALL}')
+                        valid_proxies.append(proxy)
+                        return
+                    else:
+                        write(f'{Fore.RED}[-] {Style.RESET_ALL}Geçersiz proxy: {Fore.YELLOW}{proxy}{Style.RESET_ALL}')
+                        return
+                except Exception as e:
+                    attempt += 1
+                    write(f'{Fore.RED}[-] {Style.RESET_ALL}Proxy kontrolü hatası {proxy} (Deneme {attempt}/{retries}): {e}')
+                    if attempt >= retries:
+                        write(f'{Fore.RED}[-] {Style.RESET_ALL}Proxy {proxy} için {retries} deneme sonrası hata oluştu.')
+
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            futures = [executor.submit(check_proxy, proxy.strip()) for proxy in proxylist]
+
+            for future in as_completed(futures):
+                pass
+
+        with open("valid_proxies.txt", "a") as valid_file:
+            for proxy in valid_proxies:
+                valid_file.write(f"{proxy}\n")
+
+        elapsed = time.time() - start
+        write(f'{Fore.RED}[*] {Style.RESET_ALL}Geçerli proxyler valid_proxies.txt dosyasına kaydedildi. Geçen süre: {elapsed:.2f} saniye | Kontrol edilen: {len(proxylist)} proxy')
+mdwth,watt = 'R','X', 
+def clear_screen():
+    os.system('cls' if platform.system() == "Windows" else 'clear')
+
+def set_title(title):
+    sanitized_title = re.sub(r'[^\w\s-]', '', title)
+    if platform.system() == "Windows":
+        os.system(f'title {sanitized_title}')
+    else:
+        print(f'\33]0;{sanitized_title}\a', end='', flush=True)
+
+def pause():
+    if platform.system() == "Windows":
+        os.system('pause>nul')
+    else:
+        input("Devam etmek için Enter'a basın...")
+
+def main():
+        init()
+        clear_screen()
+        print (xer),
+        classes = f'{mdwth}{sett}{mdb}{defs}{watt}{x}'
+        print(classes)
+        init(autoreset=True)
+        os.system('title Proxy Scraper') if platform.system() == 'Windows' else os.system('printf "\033]0;Proxy Scraper\007"')
+
+        xproxy = XProxy()
+        while True:
+            print(UI.menu())
+            try:
+                option = int(input(f'{Fore.YELLOW}[+] {Style.RESET_ALL}Bir seçenek girin: '))
+                if option == 1:
+                    xproxy.proxy_scrape()
+                    break
+                elif option == 2:
+                    xproxy.socks_proxy_scrape()
+                    break
+                elif option == 3:
+                    xproxy.proxy_check()
+                    break
+                else:
+                    write(f'{Fore.RED}[-] {Style.RESET_ALL}Geçersiz seçenek.')
+            except ValueError:
+                write(f'{Fore.RED}[-] {Style.RESET_ALL}Lütfen geçerli bir sayı girin.')
+
+if __name__ == "__main__":
+    main()
